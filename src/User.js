07d4dafec252035672f59957e43a0e9076303e71
Марія-Валерія Гуза <mariia-valeriia.huza@ukma.edu.ua>
2021-06-07 13:15:22 +0300

    change user

--- src/User.js
+++ src/User.js
@@ -4,6 +4,11 @@ export default class User {
     level;
     progressGame1;
 
+    progressGame2;
+
+    progressGame3;
+
+    progressGame4;
     constructor(name) {
         this.name = name;
         this.level = "";
@@ -26,19 +31,46 @@ export default class User {
         return this.progressGame1;
     }
 
-    addWordsGame1(word){
-        this.progressGame1.push(word);
-    }
-
     setProgressGame1(array){
         this.progressGame1=array;
     }
+
+
+    getProgressGame2(){
+        return this.progressGame2;
+    }
+
+    setProgressGame2(array){
+        this.progressGame2=array;
+    }
+
+
+    getProgressGame3(){
+        return this.progressGame3;
+    }
+
+    setProgressGame3(array){
+        this.progressGame3=array;
+    }
+
+    getProgressGame4(){
+        return this.progressGame4;
+    }
+
+    setProgressGame4(array){
+        this.progressGame4=array;
+    }
+
     static build(json){
         let temp = JSON.parse(json);
 
         let newUser = new User(temp.name);
         newUser.setLevel(temp.level);
         newUser.setProgressGame1(temp.progressGame1);
+        newUser.setProgressGame2(temp.progressGame2);
+        newUser.setProgressGame3(temp.progressGame3);
+        newUser.setProgressGame4(temp.progressGame4);
+
         return newUser;
     }
 
@@ -46,7 +78,10 @@ export default class User {
         return JSON.stringify({
             name:this.name,
             level:this.level,
-            progressGame1:this.progressGame1
+            progressGame1:this.progressGame1,
+            progressGame2:this.progressGame2,
+            progressGame3:this.progressGame3,
+            progressGame4:this.progressGame4
         });
     }
 }
