61341d1e1b78fe983c96ebcfcd48333c84b2ff22
Марія-Валерія Гуза <mariia-valeriia.huza@ukma.edu.ua>
2021-06-08 18:31:17 +0300

    add test database for 1 game

---
+++ src/DATABASE_FOR_ALL_GAMES.js
@@ -0,0 +1,60 @@
+///////GAME1
+
+
+//arrays for testing
+//a1
+export const words_g1_tests_a1 = ["adult", "answer", "ball", "bathroom", "camera"];
+
+export const meaning_g1_tests_a1 = ["a person or animal that has grown to full size and strength",
+    "what you say or write back to someone who has asked you a question or spoken to you",
+    "a round object that you throw, kick, or hit in a game, or something with this shape",
+    "a room with a bath and/or shower and often a toilet",
+    "a device for taking photographs or making films or television programmes"];
+
+export const sounds_g1_test_a1 = ['../Music/game1/A1/Adult.mp3','../Music/game1/A1/Answer.mp3','../Music/game1/A1/Ball.mp3','../Music/game1/A1/Bathroom.mp3','../Music/game1/A1/Camera.mp3']
+
+//a2
+export const words_g1_tests_a2 = ["accident", "across", "bad", "before", "blood"];
+
+export const meaning_g1_tests_a2 = ["something bad which happens that is not expected or intended, and which causes injury or damage",
+    "from one side of something to the other",
+    "of low quality and not acceptable",
+    "earlier than the time when something happens",
+    "the red liquid that flows around your body"];
+
+export const sounds_g1_test_a2 = ['../Music/game1/A2/Accident.mp3','../Music/game1/A2/Across.mp3','../Music/game1/A2/Bad.mp3','../Music/game1/A2/Before.mp3','../Music/game1/A2/Blood.mp3']
+
+
+//b1
+export const words_g1_tests_b1 = ["acceptable", "advertise", "benefit", "broccoli", "calf"];
+
+export const meaning_g1_tests_b1 = ["good enough",
+    "to tell people about a product or service in newspapers, on television, on the Internet, etc",
+    "something that helps you or gives you an advantage",
+    "a green vegetable with a thick stem",
+    "a young cow"];
+
+export const sounds_g1_test_b1 = ['../Music/game1/B1/Acceptable.mp3','../Music/game1/B1/Advertise.mp3','../Music/game1/B1/Benefit.mp3','../Music/game1/B1/Broccoli.mp3','../Music/game1/B1/Calf.mp3']
+
+//b2
+export const words_g1_tests_b2 = ["abandoned", "accuracy", "badge", "boast", "calculate"];
+
+export const meaning_g1_tests_b2 = ["left empty or not working",
+    "how correct or exact something is",
+    "a piece of metal, plastic, etc., with words or a picture on it which you wear on your clothes",
+    "to talk with too much pride about what you have done or what you own",
+    "to discover an amount or number using mathematics"];
+
+export const sounds_g1_test_b2 = ['../Music/game1/B2/Abandoned.mp3','../Music/game1/B2/Accuracy.mp3','../Music/game1/B2/Badge.mp3','../Music/game1/B2/Boast.mp3','../Music/game1/B2/Calculate.mp3']
+
+
+//c1
+export const words_g1_tests_c1 = ["accessory", "apply", "bankrupt", "burden", "capacity"];
+
+export const meaning_g1_tests_c1 = ["something extra which is not necessary but is attractive or useful",
+    "to use something in a particular situation",
+    "unable to continue in business because you cannot pay your debts",
+    "something difficult or unpleasant that you have to deal with or worry about",
+    "a position or job"];
+
+export const sounds_g1_test_c1 = ['../Music/game1/C1/Accessory.mp3','../Music/game1/C1/Apply.mp3','../Music/game1/C1/Bankrupt.mp3','../Music/game1/C1/Burden.mp3','../Music/game1/C1/Capacity.mp3']
